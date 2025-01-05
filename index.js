551aa30f7610ebec7bea12ae8eabe031e8c0f1d5
unknown <james.r@kalvium.community>
2025-01-05 21:16:44 +0530

    made env file

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require ("mongoose");
+const dotenv = require ("dotenv").config();
+
 
 const app = express();
 const port = 3010;
-
+app.use(express.json())
 app.use(express.static('static'));
+
+mongoose
+.connect(process.env.URL)
+.then(()=> console.log("Connected to Database"))
+.catch((err)=> console.log("Error connecting to the database:",err))
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
